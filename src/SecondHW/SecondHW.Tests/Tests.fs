4869aa32c7636b83a48859f6f7be95d69f8daeff
Ivan <ivon.klass@yandex.ru>
2022-02-26 15:29:00 +0300

    new tests

--- src/SecondHW/SecondHW.Tests/Tests.fs
+++ src/SecondHW/SecondHW.Tests/Tests.fs
@@ -1,6 +1,10 @@
 module SecondHW.Tests
 open FsCheck
 open SecondHW.NumberOfEvenNumbers
+open SecondHW.BinaryTree
+open SecondHW.ArithmeticTree
+open SecondHW.PrimeNums
+open FsUnit
 
 let numberOfEvenNumbersFunctionsShouldBeEqual x =
     let mapAndFilter = numberOfEvenNumbersMap x = numberOfEvenNumbersFilter x
@@ -11,3 +15,20 @@ let numberOfEvenNumbersFunctionsShouldBeEqual x =
     | _ -> false
 
 Check.QuickThrowOnFailure numberOfEvenNumbersFunctionsShouldBeEqual
+
+let tree =
+    BinTree.Node(4, BinTree.Empty, BinTree.Node(2, BinTree.Empty, BinTree.Empty))
+
+let newTree = mapTree tree (fun x -> x + 1)
+
+newTree |> should equal (BinTree.Node(5, BinTree.Empty, BinTree.Node(3, BinTree.Empty, BinTree.Empty)))
+
+let arithmeticTree =
+    Node(Sum, Leaf 5, Node(Multiply, Leaf 3, Leaf 2))
+
+let result = evaluate arithmeticTree
+result |> should equal 11
+
+let first10PrimeNums = [ for i in 0 .. 9 -> Seq.item i primeNums ]
+
+first10PrimeNums |> should equal [ 2; 3; 5; 7; 11; 13; 17; 19; 23; 29 ]
