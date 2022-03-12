5f5a5a9c5648bf47082b58c24677cfb7ed4625be
Ivan <ivon.klass@yandex.ru>
2022-03-12 21:46:44 +0300

    Ready version without tests

--- src/PhoneBook/PhoneBook/PhoneBook.fs
+++ src/PhoneBook/PhoneBook/PhoneBook.fs
@@ -7,30 +7,31 @@ type BookRecord =
       Phone: int64 }    
 
 let addRecord name phone book =
-    let record = { Name = name; Phone = phone }
-    record :: book
+    { Name = name; Phone = phone } :: book
     
-let findPhone name book =
-    (List.find (fun { Name = x; Phone = _ } -> x = name) book).Phone
+let findPhone name =
+    List.filter (fun { Name = x; Phone = _ } -> x = name)
+    >> List.map (fun { Name = _; Phone = x } -> x)
     
-let findName phone book =
-    (List.find (fun { Name = _; Phone = x } -> x = phone) book).Name
+let findName phone =
+    List.filter (fun { Name = _; Phone = x } -> x = phone)
+    >> List.map (fun { Name = x; Phone = _ } -> x)
     
-let printBook book =
-    List.iter (fun { Name = name; Phone = phone } -> printfn $"%s{name} %d{phone}") book
+let printBook =
+    List.iter (fun { Name = name; Phone = phone } -> printfn $"%s{name} %d{phone}")
     
-let saveBook (path: string) book =
+let saveBook (path: string) =
     use streamWriter = new StreamWriter(path)
-    List.iter (fun { Name = name; Phone = phone } -> streamWriter.WriteLine $"%s{name} %d{phone}") book
+    List.iter (fun { Name = name; Phone = phone } -> streamWriter.WriteLine $"%s{name} %d{phone}")
     
 let readBook (path: string) =
     use streamReader = new StreamReader(path)
     let rec readBookInner acc =
-        let line = streamReader.ReadLine()
+        let line = streamReader.ReadLine ()
         if line = null then acc
         else
-            let split = line.Split(" ")
-            if split.Length <> 2 then failwith "Invalid file"
+            let split = line.Split " "
+            if split.Length <> 2 then raise (FileLoadException())
             else
                 let record = { Name = split[0]; Phone = int64 split[1] }
                 readBookInner (record :: acc)
--- src/PhoneBook/PhoneBook/Program.fs
+++ src/PhoneBook/PhoneBook/Program.fs
@@ -2,8 +2,9 @@ open System
 open System.IO
 open PhoneBook
 
-let printHelp () = 
-    printfn "%s"
+let printHelp () =
+    printfn
+        "%s"
         "Hello!\n\
         Available commands for PhoneBook:\n\
         1 — exit\n\
@@ -17,41 +18,45 @@ let printHelp () =
 
 let getName () =
     printf "%s" "Please, enter name: "
-    Console.ReadLine ()
-    
+    Console.ReadLine()
+
 let rec getPhone () =
     printf "%s" "Please, enter phone: "
-    try 
-        Console.ReadLine () |> int64
-    with :? FormatException ->
+    try
+        Console.ReadLine() |> int64
+    with
+    | :? FormatException ->
         Console.WriteLine "Phone number was in incorrect format, try again"
         getPhone ()
-        
+
 let rec getValidPathToLoad () =
     printf "%s" "Please, enter path: "
-    let path = Console.ReadLine ()
-    if File.Exists path then path else
-    printfn "%s" "Path doesn't exist, try again"
-    getValidPathToLoad ()
-    
+    let path = Console.ReadLine()
+    if File.Exists path then path
+    else
+        printfn "%s" "Path doesn't exist, try again"
+        getValidPathToLoad ()
+
 let rec getValidPathToSave () =
     printf "%s" "Please, enter path: "
-    let path = Console.ReadLine ()
-    let directoryExists = path |> Path.GetDirectoryName |> Directory.Exists
-    if directoryExists then path else
-    printfn "%s" "Directory doesn't exist, try again"
-    getValidPathToSave ()
-    
+    let path = Console.ReadLine()
+    let directoryExists =
+        path |> Path.GetDirectoryName |> Directory.Exists
+    if directoryExists then path
+    else
+        printfn "%s" "Directory doesn't exist, try again"
+        getValidPathToSave ()
+
 let rec program book =
     printf "%s" "Please, enter command: "
-    match Console.ReadLine () with
+    match Console.ReadLine() with
     | "1" -> ()
     | "2" -> program (addRecord (getName ()) (getPhone ()) book)
     | "3" ->
-        printfn $"%d{findPhone (getName ()) book}"
+        List.iter (fun x -> printfn $"%d{x}") (findPhone (getName ()) book)
         program book
     | "4" ->
-        printfn $"%s{findName (getPhone ()) book}"
+        List.iter (fun x -> printfn $"%s{x}") (findName (getPhone ()) book)
         program book
     | "5" ->
         printBook book
@@ -59,10 +64,16 @@ let rec program book =
     | "6" ->
         saveBook (getValidPathToSave ()) book
         program book
-    | "7" -> program (readBook (getValidPathToLoad ()))
+    | "7" ->
+        try
+            program (readBook (getValidPathToLoad ()))
+        with
+        | :? FileLoadException | :? FormatException ->
+            printfn "%s" "Invalid file. Records must be in {name: string} {phone: int) format"
+            program book
     | _ ->
         printHelp ()
         program book
 
-printHelp ()   
+printHelp ()
 program []
